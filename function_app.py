15ba73e1c7c0c14fc0f77cf0d1658441f472bf7e
V-Castro-Alves <vitor.castro.2532@gmail.com>
2026-09-22 16:16:23 -0300

    Add timer trigger function to call HTTP trigger and log response

--- function_app.py
+++ function_app.py
@@ -1,4 +1,7 @@
 import logging
+from urllib.parse import urlencode
+from urllib.request import urlopen
+
 import azure.functions as func
 
 app = func.FunctionApp()
@@ -24,3 +27,15 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def timer_trigger_http_call(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    url = 'https://funcapp-vitor0001-egbfcmd9epcyamhb.eastus2-01.azurewebsites.net/api/http_trigger'
+    query = urlencode({'name': 'Information sent by timer trigger'})
+
+    with urlopen(f'{url}?{query}', timeout=10) as response:
+        logging.info('HTTP function response: %s', response.read().decode('utf-8'))
